746950a9f25d5ee212a5ae0bade756810eaffc86
Pedro Júnior <pedrojuniorifpb@gmail.com>
2021-02-14 00:41:59 -0300

    Corrigindo uns problemas
    A versão anterior estava dando uns problemas bem chatos, nessa houver uma correção

--- cranio_new.py
+++ cranio_new.py
@@ -1,5 +1,4 @@
 from Segunda_parte import segunda_parte
-#O nome será esse temporariamente
 from imagens import carregar_imagens
 dicionario_lista={158:0,156:1,153:2,155:3,150:4,157:5,152:6,149:7,151:8,154:9,8:12345}
 #Dicionario criado para que seja possivel capturar as chaves do teclado que será usado no IFPB
@@ -7,35 +6,36 @@ dicionario_lista={158:0,156:1,153:2,155:3,150:4,157:5,152:6,149:7,151:8,154:9,8:
 while True:
     matricula=""
     cor=(255,255,255)
-    recolher_matricula()
-    if saida==-1:
+    chave = carregar_imagens("cranio.png").mostrar_imagem(0)
+    """
+    if chave==-1:
         break
-    segunda_parte(matricula_sorteio)
+    segunda_parte(matricula)
     while True:
-        self.imagem_pricipal()
-        if self.saida==-1 or (self.saida==13 and len(self.matricula_sorteio)==12):
+        if chave==-1 or (chave==13 and len(matricula)==12):
             break
-        elif self.saida!=8:
-            if len(self.matricula_sorteio)<12:
-                if self.saida==13:
+        elif chave!=8:
+            if len(matricula)<12:
+                if chave==13:
                     continue
-                elif self.saida<100:
-                    self.matricula_sorteio+=str(self.saida-48)
+                elif chave<100:
+                    matricula+=str(chave-48)
                 else:
                     try:
-                        self.matricula_sorteio+=str(self.dicionario_lista[self.saida])
+                        matricula+=str(dicionario_lista[chave])
                     except:
                         continue
         else:
             try:
-                self.matricula_sorteio = int(self.matricula_sorteio)/10
-                self.matricula_sorteio = int(self.matricula_sorteio)
-                self.matricula_sorteio = str(self.matricula_sorteio)
+                matricula = int(matricula)/10
+                matricula = int(matricula)
+                matricula = str(matricula)
             except:
                 continue
-            if self.matricula_sorteio=="0":
-                self.matricula_sorteio=""
-        if len(self.matricula_sorteio)==12:
-            self.cor=(0,255,0)
+            if matricula=="0":
+                matricula=""
+        if len(matricula)==12:
+            cor=(0,255,0)
         else:
-            self.cor=(255,255,255)
+            cor=(255,255,255)
+    """
--- imagens.py
+++ imagens.py
@@ -3,9 +3,9 @@ from os import sep
 #Aqui será o unico lugar que irá usar 'os'
 class carregar_imagens:
     def __init__(self, imagem):
-        self.imagem = imread("imagens_funcionamento"+sep+"cranio.png")
+        self.imagem = cv2.imread("imagens_funcionamento"+sep+imagem)
     
     def mostrar_imagem(self, tempo):
         cv2.imshow("Cranio",self.imagem)
-		saida = cv2.waitKey(tempo)
+        saida = cv2.waitKey(tempo)
         return saida
