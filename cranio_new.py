47c3de29fd2f50e7a190c5ce3f073968f0859c71
Pedro Júnior <pedrojuniorifpb@gmail.com>
2021-02-14 01:47:14 -0300

    Atualização
    Atualizei umas coisas e corrigir um erro da dado nas imagens

--- cranio_new.py
+++ cranio_new.py
@@ -3,39 +3,42 @@ from imagens import carregar_imagens
 dicionario_lista={158:0,156:1,153:2,155:3,150:4,157:5,152:6,149:7,151:8,154:9,8:12345}
 #Dicionario criado para que seja possivel capturar as chaves do teclado que será usado no IFPB
 #Um dia capturando pockemom, e outro chaves, onde vamos parar?
+matricula = ""
+cor=(255,255,255)
 while True:
-    matricula=""
-    cor=(255,255,255)
-    chave = carregar_imagens("cranio.png").mostrar_imagem(0)
-    """
-    if chave==-1:
+    chave = int(carregar_imagens("cranio.jpg").mostrar_imagem(0))
+    print(matricula)
+    #Nessa linha se carrega a imagem ao mesmo tempo que recolhe a chave do teclado
+    if chave==-1 or (chave==13 and len(matricula)==12):
         break
-    segunda_parte(matricula)
-    while True:
-        if chave==-1 or (chave==13 and len(matricula)==12):
-            break
-        elif chave!=8:
-            if len(matricula)<12:
-                if chave==13:
-                    continue
-                elif chave<100:
-                    matricula+=str(chave-48)
-                else:
-                    try:
-                        matricula+=str(dicionario_lista[chave])
-                    except:
-                        continue
-        else:
-            try:
-                matricula = int(matricula)/10
-                matricula = int(matricula)
-                matricula = str(matricula)
-            except:
+        #Isso aqui verifica a matricula já possui o tamanho de 13 caracteres e se foi apertado o enter
+        #A chave 8 se referi a o BlackEspace
+    elif chave!=8:
+        if len(matricula)<12:
+            if chave==13:
                 continue
-            if matricula=="0":
-                matricula=""
-        if len(matricula)==12:
-            cor=(0,255,0)
-        else:
-            cor=(255,255,255)
-    """
+                #Mais um enter, aqui ele só não reage ao que o usuario pede, por ter menos de 13 caracteres
+            elif chave<100:
+                matricula+=str(chave-48)
+            else:
+                try:
+                    matricula+=str(dicionario_lista[chave])
+                except:
+                    continue
+    else:
+        try:
+            matricula = int(int(matricula)/10)
+            #Apaga um número da matricula escrita
+            matricula = str(matricula)
+            #Volta ao formato que estava sendo trabalhado antes
+        except:
+            continue
+        if matricula=="0":
+            matricula=""
+    if len(matricula)==12:
+        cor=(0,255,0)
+    else:
+        cor=(255,255,255)
+    #Desculpa, pretendo colocar todas os comentarios depois
+    #Depois verei uma melhor forma de tirar isso tudo daqui
+    #Além de que pretendo Escrever na imagem de entrada, isso já tem feito no Primeira_parte, mas irei colocar junto com o arquivo de imagens.py
--- imagens.py
+++ imagens.py
@@ -4,8 +4,12 @@ from os import sep
 class carregar_imagens:
     def __init__(self, imagem):
         self.imagem = cv2.imread("imagens_funcionamento"+sep+imagem)
+        #Aqui ele fará a leitura da iamgem, coloquei para trabalhar com self pelo motivo que será reusado
     
     def mostrar_imagem(self, tempo):
         cv2.imshow("Cranio",self.imagem)
+        #Aqui demostra a imagem
         saida = cv2.waitKey(tempo)
+        #Aqui irá recolher a chave apertada pelo usuario
         return saida
+        #Retorna uma string
